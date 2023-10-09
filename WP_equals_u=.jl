6be3b6ad2d8bebdd55fc91854f9f7da4d267550a
minboli <144169107+minboli@users.noreply.github.com>
2023-10-09 13:03:37 -0500

    revise matrix

--- WP_equals_u=.jl
+++ WP_equals_u=.jl
@@ -22,7 +22,7 @@ W = read(file_W,"W")
 file_b = matopen(raw"C:\Users\minboli\Desktop\matlab data\Xincode\Xincode\b.mat")
 b = read(file_b,"b")
 
-file_D = matopen(raw"C:\Users\minboli\Desktop\matlab data\Xincode\Xincode\D.mat")
+file_D = matopen(raw"C:\Users\minboli\Desktop\matlab data\Xincode\Xincode\sparse_matrix_D.mat")
 D = read(file_D,"D")
 
 @variable(model, p_PV_3d_u[1:N_PV_3d, 1:num_time])
@@ -45,16 +45,12 @@ D = read(file_D,"D")
 
 
 function constraints_on_WP_lessthan_or_equalsto_U(m, upper_vars, lower_vars, W, D, u, b)
-#m refers to model, W*P<=U,P0=D*P+b
-    # upper_reshape = vcat(upper_vars...)
-    # #joint elements in upper_vars together
-    # lower_reshape = vcat(lower_vars...)
-    # #joint elements in lower_vars together
+    #model W*P<=U,P0=D*P+b
     @constraint(m,W * upper_vars .<= u )
     @constraint(m,W * lower_vars .<= u )
     #guarantee every element in W*P lower than U
-    @constraint(m, p0_u .== D * upper_vars + b) 
-    @constraint(m, p0_l .== D * lower_vars + b)
+    @constraint(m, reshape(p0_u,:,1) .== D * upper_vars + b) 
+    @constraint(m, reshape(p0_l,:,1) .== D * lower_vars + b)
     #guarantee P0 follow P0= DP+b
     @constraint(m, p_bat_3d_l .>= p_bat_3d_u)
     @constraint(m, p_batagg_3d_l .>= p_batagg_3d_u)
@@ -67,10 +63,14 @@ upper_vars = vcat([p_PV_3d_u; p_PV_1d_u; p_PV_1w_u; p_PVagg_3d_u; p_bat_3d_u; p_
 lower_vars = vcat([p_PV_3d_l; p_PV_1d_l; p_PV_1w_l; p_PVagg_3d_l; p_bat_3d_l; p_batagg_3d_l; p_HVAC_3d_l])[:]
 constraints_on_WP_lessthan_or_equalsto_U(model, upper_vars, lower_vars, W, D, u, b)
 
+
 len_u = length(upper_vars)
 len_l = length(lower_vars)
 f = zeros(len_u)' .* upper_vars + zeros(len_l)' .* lower_vars
 @objective(model, Max, f)
 
 optimize!(model)
-
+print("Optimal value of upper_vars",value.(upper_vars))
+print("Optimal value of lower_vars",value.(lower_vars))
+print("Optimal value of p0_l",value.(p0_l))
+print("Optimal value of p0_u",value.(p0_u))
